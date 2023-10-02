34765dfd9add3e51acd4f9ba2e3d0cd9725ef6b6
Jasmitsingh01 <97171634+Jasmitsingh01@users.noreply.github.com>
2023-10-02 15:13:43 +0530

    All

--- Chat App/Chat Server/Models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose  = require("mongoose");
-
-const User = new mongoose.Schema({
-    Eamil:String,
-    Password:String,
-    contact_No:String
-});
-module.exports= mongoose.model('users', User)
--- Chat App/Chat Server/Routes/Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { Singup, Login } = require("../Controllers/User's");
-const routes=express.Router();
-
-routes.post('/singup',Singup)
-routes.post('/login',Login)
-
-module.exports=routes;
